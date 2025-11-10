bc8c7fa2a43485cb7e4849fd612bb8eef4ec6826
RomannRoss <rscharipov@gmail.com>
2025-11-10 17:45:03 +0500

    Replaced driver with browser, added boolean, created a folder for chromedriver.exe

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,39 +4,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest {
-    private WebDriver driver;
+    private WebDriver browser;
 
     @BeforeEach
     void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        browser = new ChromeDriver();
+        browser.manage().window().maximize();
     }
 
     @Test
     void test1_positiveLogin() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-        String title = driver.getTitle();
-        assertEquals("Swag Labs", title);
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.id("user-name")).sendKeys("standard_user");
+        browser.findElement(By.id("password")).sendKeys("secret_sauce");
+        browser.findElement(By.id("login-button")).click();
+        WebElement header = browser.findElement(By.cssSelector(".title"));
+        boolean isHeaderVisible = header.isDisplayed();
+        assertTrue(isHeaderVisible, "Заголовок 'Swag Labs' должен быть виден на экране");
+        assertEquals("Swag Labs", header.getText());
     }
 
     @Test
     void test2_negativeLogin() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("wrong_user");
-        driver.findElement(By.id("password")).sendKeys("wrong_pass");
-        driver.findElement(By.id("login-button")).click();
-        String error = driver.findElement(By.xpath("//h3[@data-test='error']")).getText();
-        assertEquals("Epic sadface: Username and password do not match any user in this service", error);
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.id("user-name")).sendKeys("wrong_user");
+        browser.findElement(By.id("password")).sendKeys("wrong_pass");
+        browser.findElement(By.id("login-button")).click();
+        WebElement errorElement = browser.findElement(By.xpath("//h3[@data-test='error']"));
+        boolean isErrorVisible = errorElement.isDisplayed();
+        assertTrue(isErrorVisible, "Сообщение об ошибке должно быть видно");
+        assertEquals("Epic sadface: Username and password do not match any user in this service",
+                errorElement.getText());
     }
 
     @AfterEach
     void tearDown() {
-        if (driver != null) {
-            driver.quit();
+        if (browser != null) {
+            browser.quit();
         }
     }
 }
+
