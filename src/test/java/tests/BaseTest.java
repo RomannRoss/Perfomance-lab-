e001d5eabf8902804d9d071f11c64069010dbace
RomannRoss <rscharipov@gmail.com>
2025-11-17 16:49:11 +0500

    Added cross_browser.xml, corrected BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,18 +3,30 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
 public class BaseTest {
+
     public WebDriver browser;
 
     @BeforeMethod
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        browser = new ChromeDriver();
+    @Parameters("browser")
+    public void setup(@Optional("chrome") String browserName) {
+
+        if ("edge".equalsIgnoreCase(browserName)) {
+            WebDriverManager.edgedriver().setup();
+            browser = new EdgeDriver();
+        } else {
+            WebDriverManager.chromedriver().setup();
+            browser = new ChromeDriver();
+        }
+
         browser.manage().window().maximize();
         browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
